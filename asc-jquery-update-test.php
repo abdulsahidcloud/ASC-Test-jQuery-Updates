996004c04a650913bf4e4e4113d966d9063de4f5
abdulsahidcloud <63746743+abdulsahidcloud@users.noreply.github.com>
2020-10-06 23:09:57 +0530

    Add files via upload

---
+++ asc-jquery-update-test.php
@@ -0,0 +1,7 @@
+<?php /*Plugin Name: ASC Test jQuery Updates
+ * Plugin URI: https://github.com/abdulsahidcloud/ASC-Test-jQuery-Updates
+ * Description: A feature plugin to help with testing updates of the jQuery and jQuery UI JavaScript libraries 
+ * (not intended for use in production).
+ * Version: 1.0.1 Requires at least: 5.4 Tested up to: 5.5 Requires PHP: 5.6 Author: Abdul sahid cloud
+ * License: GPLv2 License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: ASC-jquery-update-test Network: true*/include_once __DIR__ . '/class_asc_jquery_update_test.php';
